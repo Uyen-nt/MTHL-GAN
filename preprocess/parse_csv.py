83afd28fddcd2ae38dfc1da7dda56164470d627b
Uyen-nt <uyenntse170340@fpt.edu.vn>
2025-10-14 16:28:03 +0700

    Update parse_csv.py

--- preprocess/parse_csv.py
+++ preprocess/parse_csv.py
@@ -174,7 +174,7 @@ class Mimic3Parser(EHRParser):
         return filename, cols, converter
 
     def set_procedure(self):
-        filename = 'D_ICD_PROCEDURES.csv'
+        filename = 'PROCEDURES_ICD.csv'  # <-- đây mới là file chứa SUBJECT_ID, HADM_ID
         cols = {self.pid_col: 'SUBJECT_ID', self.adm_id_col: 'HADM_ID', self.cid_col: 'ICD9_CODE'}
         converter = {'SUBJECT_ID': int, 'HADM_ID': int, 'ICD9_CODE': str}
         return filename, cols, converter
